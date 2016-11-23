05174a199cdf848dd13fdd3252e3392557a424e3
Edward Park <ed.sh.park@gmail.com>
2016-11-22 22:08:25 -0800

    Implemented Socket -> Queue methods, tested w demo app

--- demo/server/server.js
+++ demo/server/server.js
@@ -1,12 +1,14 @@
+// const io = require('socket.io')(http);
+// const Razorframe = require('../../lib/Razorframe.js');
+// const rz = Razorframe(http);
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
-// const io = require('socket.io')(http);
-
-// const Razorframe = require('../../lib/Razorframe.js');
-// const rz = Razorframe(http);
+const rb = require('../../lib/Razorbrain.js');
+rb(http);
 
 const { addToDb, showAll } = require('./eventCtrl');
 
@@ -14,35 +16,12 @@ app.use(express.static(path.join(__dirname, '../'))); // need this to be able to
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
 // Routing --------------------------------
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-// app.post('/addToDb', addToDb);
 
 
-
-//-------------------------------
-//import Razorframe
-
-function importedFn(http) {
-  const io = require('socket.io')(http);
-  io.on('connection', (socket) => {
-    console.log('a user connected!');
-    showAll();
-    
-    socket.on('disconnect', () => {
-      // console.log('user disconnected!');
-    });
-
-    socket.on('text entry', (msg) => {
-      addToDb(msg);
-      // io.emit('text receieved', msg);
-      socket.broadcast.emit('text receieved', msg);
-    });
-  })
-}
 
 http.listen(3000, () => console.log('✌️  on 3000'));
