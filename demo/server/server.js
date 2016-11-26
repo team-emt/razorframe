5b97473902cff38bf72ef49527fc6cd8d17a2719
Edward Park <ed.sh.park@gmail.com>
2016-11-25 18:40:21 -0800

    added DB Write to Dequeue method

--- demo/server/server.js
+++ demo/server/server.js
@@ -8,50 +8,62 @@ const app = express();
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 
+const { addToDb, showAll } = require('./eventCtrl'); // db methods
+
 const rb = require('../../lib/Razorbrain.js');
-rb(http);
 
-// const { addToDb, showAll } = require('./eventCtrl');
+// let params =  { http: http, writeCallback: addToDb, pullCallback: showAll }
+// start thinking about parameter structure for anticipated callback needs
 
-const { addToDb, showAll } = require('./eventCtrl');
-const rz = require('../../lib/Razorframe.js');
-// rb(http);
+rb(http, addToDb);
 
-const io = require('socket.io')(http);
 
-app.use(express.static(path.join(__dirname, '../'))); // need this to be able to serve up app.js + styles.css
+// need this to be able to serve up app.js + styles.css
+app.use(express.static(path.join(__dirname, '../')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// Routing --------------------------------
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected!');
 
-    socket.on('disconnect', () => {
-      console.log('user disconnected!');
-    });
+
+
+
+
+
+
+
+// -------------------------------------------------------
+// When we moved the Socket connection block over to server.js
+
+// const io = require('socket.io')(http);
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected!');
+
+//     socket.on('disconnect', () => {
+//       console.log('user disconnected!');
+//     });
 
     
-    // Event listeners below 
-    socket.on('event', (msg) => {
-      // io.emit('text receieved', msg);
-      // socket.broadcast.emit('text receieved', msg);
-      msg.socket = socket;
-      msg.io = io;
+//     // Event listeners below 
+//     socket.on('event', (msg) => {
+//       // io.emit('text receieved', msg);
+//       // socket.broadcast.emit('text receieved', msg);
+//       msg.socket = socket;
+//       msg.io = io;
 
-      rz.enqueue(msg);
-      // rz.broadcastOthers(socket, msg);
+//       rz.enqueue(msg);
+//       // rz.broadcastOthers(socket, msg);
 
-    });
-    // socket.on('sent', msg => {
-    //   console.log('inside db listener');
-    //   addToDb(msg);
-    // });
-  });
+//     });
+    
+//     socket.on('sent', msg => {
+//       console.log('inside db listener');
+//       addToDb(msg);
+//     });
+//   });
 
 
 
