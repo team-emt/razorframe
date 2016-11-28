d7971ae1cbf66c5dabd39894976b61231145404e
Edward Park <ed.sh.park@gmail.com>
2016-11-28 14:18:52 -0800

    LETS REVIEW ON MONDAY AM (#5)
    Feat: adds enq + dq functionality within RF class constructor
    
    Feat: adds enq + dq functionality; socket emit methods within RF class constructor
    
    Feat: adds DB Write to Dequeue method
    
    Feat: adds OOP-centered argument patters to pass down necessary fn's
    
    Feat: adds server-side listeners using EE

--- demo/server/server.js
+++ demo/server/server.js
@@ -1,52 +1,79 @@
-// const io = require('socket.io')(http);
-// const Razorframe = require('../../lib/Razorframe.js');
-// const rz = Razorframe(http);
-
 const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 
-const { addToDb, showAll } = require('./eventCtrl');
-const rz = require('../../lib/Razorframe.js');
-// rb(http);
+const { addToDb, showAll } = require('./eventCtrl'); // user-defined db methods
 
-const io = require('socket.io')(http);
 
-app.use(express.static(path.join(__dirname, '../'))); // need this to be able to serve up app.js + styles.css
+
+
+const rb = require('../../lib/Razorbrain.js');
+
+/**
+ * NEURON parameters - passes into rb the http object + any user-defined callbacks
+ * @param - {Object} http => instantiate an http server
+ * @param - {Function} write => a DB write callback (user-defined)
+ * @param - {Function} show => a DB pull callback (user-defined)
+ */
+
+const NEURON = {
+  http,
+  write: addToDb,
+  show: null,
+};
+
+rb(NEURON); // could also pass http as first arg, NEURON as second arg with ONLY callbacks
+
+
+
+
+app.use(express.static(path.join(__dirname, '../')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// Routing --------------------------------
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected!');
 
-    socket.on('disconnect', () => {
-      console.log('user disconnected!');
-    });
+
+
+
+
+
+
+
+// -------------------------------------------------------
+// When we moved the Socket connection block over to server.js
+
+// const io = require('socket.io')(http);
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected!');
+
+//     socket.on('disconnect', () => {
+//       console.log('user disconnected!');
+//     });
 
     
-    // Event listeners below 
-    socket.on('event', (msg) => {
-      // io.emit('text receieved', msg);
-      // socket.broadcast.emit('text receieved', msg);
-      msg.socket = socket;
-      msg.io = io;
+//     // Event listeners below 
+//     socket.on('event', (msg) => {
+//       // io.emit('text receieved', msg);
+//       // socket.broadcast.emit('text receieved', msg);
+//       msg.socket = socket;
+//       msg.io = io;
 
-      rz.enqueue(msg);
-      // rz.broadcastOthers(socket, msg);
+//       rz.enqueue(msg);
+//       // rz.broadcastOthers(socket, msg);
 
-    });
-    // socket.on('sent', msg => {
-    //   console.log('inside db listener');
-    //   addToDb(msg);
-    // });
-  });
+//     });
+    
+//     socket.on('sent', msg => {
+//       console.log('inside db listener');
+//       addToDb(msg);
+//     });
+//   });
 
 
 
