b46fb8d7b1383578713daceb2b740c6bddc62812
Michael Sotkin <michael.sotkin@gmail.com>
2016-11-29 11:12:41 -0800

    resolve merge conflicts

--- demo/server/server.js
+++ demo/server/server.js
@@ -9,7 +9,7 @@ const { addToDb, showAll } = require('./eventCtrl'); // user-defined db methods
 
 
 
-const rb = require('../../lib/Razorbrain.js');
+// const rb = require('../../lib/Razorbrain.js');
 
 /**
  * NEURON parameters - passes into rb the http object + any user-defined callbacks
@@ -17,13 +17,29 @@ const rb = require('../../lib/Razorbrain.js');
  * @param - {Function} write => a DB write callback (user-defined)
  * @param - {Function} show => a DB pull callback (user-defined)
  */
-const NEURON = {
-  write: addToDb,
-  show: showAll,
-};
+// const NEURON = {
+//   write: addToDb,
+//   show: showAll,
+// };
 
-rb(http, NEURON);
+// rb(http, NEURON);
+const io = require('socket.io')(http);
 
+io.on('connection', (socket) => {
+    console.log('a user connected!');
+    showAll(socket);
+    socket.on('disconnect', () => {
+      console.log('user disconnected!');
+    });
+
+    
+    // Event listeners below 
+    socket.on('event', (msg) => {
+      console.log(msg);
+      addToDb(msg.contents);
+      socket.emit(msg.eventOut, msg.contents);
+    });
+  });
 
 app.use(express.static(path.join(__dirname, '../')));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,34 +59,7 @@ app.get('/', (req, res) => {
 // -------------------------------------------------------
 // When we moved the Socket connection block over to server.js
 
-// const io = require('socket.io')(http);
-
-// io.on('connection', (socket) => {
-//     console.log('a user connected!');
-
-//     socket.on('disconnect', () => {
-//       console.log('user disconnected!');
-//     });
-
-    
-//     // Event listeners below 
-//     socket.on('event', (msg) => {
-//       // io.emit('text receieved', msg);
-//       // socket.broadcast.emit('text receieved', msg);
-//       msg.socket = socket;
-//       msg.io = io;
-
-//       rz.enqueue(msg);
-//       // rz.broadcastOthers(socket, msg);
-
-//     });
-    
-
-//     socket.on('sent', msg => {
-//       console.log('inside db listener');
-//       addToDb(msg);
-//     });
-//   });
+// 
 
 
 
