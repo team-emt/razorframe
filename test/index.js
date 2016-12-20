96c02e9fb809d3d82d275fd5196359d90973e939
Edward Park <ed.sh.park@gmail.com>
2016-12-20 11:42:08 -0800

    Test: Adds Tests for Messaging Queue

---
+++ test/index.js
@@ -0,0 +1,8 @@
+// require('babel-register')({
+//   presets: ['es2015', 'react'],
+// });
+
+require('./js/unit');
+// require('./js/supertest');
+// require('./js/zombie');
+// require('./js/enzyme');
