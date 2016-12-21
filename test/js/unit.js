2d580ba4fd1e3622e00bf015ab199fa4489fa86e
Edward Park <ed.sh.park@gmail.com>
2016-12-20 16:41:59 -0800

    Tests: Updated MQ tests

--- test/js/unit.js
+++ test/js/unit.js
@@ -1,6 +1,7 @@
 const LinkedList = require('../../lib/LinkedList');
-const rz = require('../../lib/Razorframe');
+const { rz } = require('../../lib/Razorframe');
 const { expect } = require('chai');
+const sinon = require('sinon');
 
 describe('messaging queue unit tests', () => {
 
@@ -25,5 +26,28 @@ describe('messaging queue unit tests', () => {
     });
   });
 
+  describe('messaging queue tests', () => {
+
+    it('Razorframe should be instantiated', () => {
+      expect(rz.razorframe).to.not.equal(undefined);
+    });
+
+    it('Should be empty', () => {
+      expect(rz.razorframe.storage.length).to.equal(0);
+    });
+
+    it('should enqueue and dequeue messages', () => {
+      let obj1 = { contents: 'test1', eventOut: 'test', action: 'write' };
+      let obj2 = { contents: 'test2', eventOut: 'test', action: 'write' };
+      rz.razorframe.enqueue(obj1);
+      expect(rz.razorframe.storage.length).to.equal(1);
+      rz.razorframe.enqueue(obj2);
+      expect(rz.razorframe.storage.length).to.equal(2);
+      expect(rz.razorframe.storage.pop()).to.equal(obj1);
+      expect(rz.razorframe.storage.length).to.equal(1);
+      expect(rz.razorframe.storage.pop()).to.equal(obj2);
+      expect(rz.razorframe.storage.length).to.equal(0);
+    });
+  });
 });
 
