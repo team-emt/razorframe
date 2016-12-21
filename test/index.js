f542b7bc4ce0ec4e3b22ac2531bf1bfd8e863143
Edward Park <ed.sh.park@gmail.com>
2016-12-20 19:30:35 -0800

    Chore: cleaned up test entry file

--- test/index.js
+++ test/index.js
@@ -1,8 +1,2 @@
-// require('babel-register')({
-//   presets: ['es2015', 'react'],
-// });
 
 require('./js/unit');
-// require('./js/supertest');
-// require('./js/zombie');
-// require('./js/enzyme');
