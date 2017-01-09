d290d8b0f8a770311f9f4d3af8fa24dc19f15427
Edward Park <edwardpark@Edwards-MacBook-Pro.local>
2017-01-09 14:49:13 -0800

    Cleaning up test entrypoint; removing comments

--- test/index.js
+++ test/index.js
@@ -1,16 +1,14 @@
-// const Promise = require('bluebird');
 
-// const unit = Promise.promisify(require('./js/unit'));
 const unit = require('./js/unit');
 const integration = require('./js/integration');
 
 unit.then(function(result) {
   integration();
 }).catch(() => {
-  console.log('caught!');
+  console.log('error with unit test promise');
 });
 
-// require('./js/integration')
+
 
 
 
